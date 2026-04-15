5ff375cb487bdaec9f942110437b0cb34477c1e4
tarstarcas@gmail.com <ivan.major.888@gmail.com>
2026-04-15 15:03:05 +0300

    Solution

--- src/createServer.js
+++ src/createServer.js
@@ -1,8 +1,37 @@
 'use strict';
 
+const http = require('http');
+
 function createServer() {
-  /* Write your code here */
-  // Return instance of http.Server class
+  return http.createServer((req, res) => {
+    if (req.method === 'POST' && req.url === '/add-expense') {
+      let body = '';
+
+      req.on('data', (chunk) => {
+        body += chunk.toString();
+      });
+
+      req.on('end', () => {
+        const expense = JSON.parse(body);
+
+        if (expense.date && expense.title && expense.amount) {
+          const fs = require('fs');
+          const path = require('path');
+          const dataPath = path.resolve(__dirname, '../db/expense.json');
+
+          fs.writeFileSync(dataPath, JSON.stringify(expense));
+          res.writeHead(200, { 'Content-Type': 'application/json' });
+          res.end(JSON.stringify(expense));
+        } else {
+          res.writeHead(400, { 'Content-Type': 'application/json' });
+          res.end(JSON.stringify(['Missing required fields']));
+        }
+      });
+    } else {
+      res.writeHead(404, { 'Content-Type': 'application/json' });
+      res.end(JSON.stringify({ error: 'Not Found' }));
+    }
+  });
 }
 
 module.exports = {
